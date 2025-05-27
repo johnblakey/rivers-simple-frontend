4dd7ddf3e15d241f93f1ad893873fb98a61a648c
johnblakey <4926778+johnblakey@users.noreply.github.com>
2025-05-27 00:48:44 -0700

    Added TypeScript config files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,4 +8,12 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
+  // Add this configuration object for ignores
+  {
+    ignores: [
+      "dist/",
+      "node_modules/",
+      // You can add other files or directories here if needed
+    ],
+  },
 ]);
---
+++ main.js
@@ -0,0 +1,8 @@
+// main.ts
+function greeter(person) {
+    return "Hello, " + person;
+}
+const user = "John Blakey";
+document.body.textContent = greeter(user);
+export {};
+//# sourceMappingURL=main.js.map
